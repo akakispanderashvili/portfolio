a696e1564b92875802d3c33cc259b7081d23bb8e
akakispanderashvili <86373282+akakispanderashvili@users.noreply.github.com>
2022-03-16 17:19:04 +0400

    Add files via upload

---
+++ assets/images/img/assets/js/script.js
@@ -0,0 +1,9 @@
+function openImage(){
+    let image = document.getElementById('image-1');
+    let style = image.style;
+    style = "absolute";
+    style.height = "80%";
+    style.width = "auto";
+    style.top = "0"
+    style.left = "50%"
+}
